2ea05d4fe3e751e939d264265fa5ebcebe9dddb5
Jimmy Andrade <contato@jimmyandrade.com>
2024-12-14 16:31:57 -0300

    feat: add ConcertsPage component for displaying concert shows with header and list

---
+++ apps/dotmusic/src/app/shows/page.tsx
@@ -0,0 +1,17 @@
+import { ConcertsList } from '../../libs/concerts/feature/ConcertsList';
+import { Container } from '../../libs/shared/ui/Container';
+import { PageHeader } from '../../libs/shared/ui/PageHeader';
+import { PageHeading } from '../../libs/shared/ui/PageHeading';
+
+export default async function ShowsPage() {
+  return (
+    <article>
+      <PageHeader>
+        <PageHeading>Shows</PageHeading>
+      </PageHeader>
+      <Container>
+        <ConcertsList />
+      </Container>
+    </article>
+  );
+}
