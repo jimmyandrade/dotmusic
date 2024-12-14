d629d65420e4faf675c560ff15fbcb76060b82a3
Jimmy Andrade <contato@jimmyandrade.com>
2024-12-14 16:32:22 -0300

    feat: update BioPage layout with new components and improved biography content

--- apps/dotmusic/src/app/bio/page.tsx
+++ apps/dotmusic/src/app/bio/page.tsx
@@ -1,17 +1,38 @@
-import { Container, Heading } from '@radix-ui/themes';
+import { Flex, Text } from '@radix-ui/themes';
+import { Container } from '../../libs/shared/ui/Container';
+import { PageHeader } from '../../libs/shared/ui/PageHeader';
+import { PageHeading } from '../../libs/shared/ui/PageHeading';
 
 export default function BioPage() {
   return (
-    <article>
+    <article id={BioPage.name}>
+      <PageHeader>
+        <PageHeading>
+          Quem é <strong>Jimmy Andrade</strong>?
+        </PageHeading>
+      </PageHeader>
       <Container>
-        <Heading as="h2">Biografia</Heading>
-        <p>
-          <strong>Jimmy Andrade</strong> é uma pessoa cantora, compositora e
-          produtora musical. Com nascimento em{' '}
-          <time dateTime="1989-05-27">27 de maio de 1989</time> em Minas Novs,
-          uma cidade localizada na região do Vale do Jequitinhonha, em Minas
-          Gerais, conhecida por sua riqueza cultural.
-        </p>
+        <Flex direction="column" gapY="4">
+          <Text
+            as="div"
+            className="max-w-prose"
+            size={{
+              initial: '2',
+              xs: '3',
+              sm: '4',
+              md: '5',
+              lg: '6',
+              xl: '7',
+            }}
+          >
+            <p>
+              Cantor, compositor e produtor musical. Nasceu em{' '}
+              <time dateTime="1989-05-27">27 de maio de 1989</time> em Minas
+              Novas, uma cidade localizada na região do Vale do Jequitinhonha,
+              em Minas Gerais, conhecida por sua riqueza cultural.
+            </p>
+          </Text>
+        </Flex>
       </Container>
     </article>
   );
