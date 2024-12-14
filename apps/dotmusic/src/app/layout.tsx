9f8c130652fe3b1ed0c2852beda49f3f46794e74
Jimmy Andrade <contato@jimmyandrade.com>
2024-12-14 16:33:20 -0300

    feat: refactor RootLayout to use SiteHeader, MainContent, and SiteFooter components

--- apps/dotmusic/src/app/layout.tsx
+++ apps/dotmusic/src/app/layout.tsx
@@ -1,18 +1,18 @@
-import {
-  Box,
-  Container,
-  Flex,
-  Heading,
-  TabNav,
-  Theme,
-  VisuallyHidden,
-} from '@radix-ui/themes';
-import Link from 'next/link';
+import { Theme } from '@radix-ui/themes';
 import '@radix-ui/themes/styles.css';
 import './global.css';
 import type { Metadata, Viewport } from 'next';
-import { SocialMediaLinks } from '../features/social-media-links';
+import { Albert_Sans } from 'next/font/google';
+import { SiteFooter } from '../libs/shared/ui/SiteFooter';
+import { SiteHeader } from '../libs/shared/ui/SiteHeader';
+import { MainContent } from '../libs/shared/ui/MainContent';
+import { type ReactNode } from 'react';
 
+const albertSans = Albert_Sans({
+  display: 'swap',
+  subsets: ['latin'],
+  variable: '--font-albert-sans',
+});
 const applicationName = 'Jimmy Andrade';
 
 export const metadata: Metadata = {
@@ -20,10 +20,6 @@ export const metadata: Metadata = {
     'Jimmy Andrade - Website oficial: Música, Agenda de Shows, Vídeos, Notícias e Biografia',
   description:
     'Website oficial do cantor, compositor e produtor musical Jimmy Andrade. Artista de indie, pop e rock brasileiro. Escute suas músicas, consulte sua agenda e obtenha outras informações sobre a sua música.',
-  appleWebApp: {
-    capable: true,
-    title: applicationName,
-  },
   applicationName,
   openGraph: {
     siteName: applicationName,
@@ -36,65 +32,18 @@ export const viewport: Viewport = {
   width: 'device-width',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export interface RootLayoutProps {
+  children: ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <Theme asChild appearance="dark" hasBackground>
-      <html lang="pt-br">
-        <body>
-          <Box asChild position="fixed" width="100%" className="z-10 bg-black">
-            <header>
-              <Container>
-                <Flex>
-                  <Heading as="h1" size="1">
-                    <Link href="/">Jimmy Andrade</Link>
-                  </Heading>
-                  <VisuallyHidden asChild>
-                    <a href="#content">Pular para o conteúdo principal</a>
-                  </VisuallyHidden>
-                  <TabNav.Root aria-label="Navegação principal">
-                    <TabNav.Link asChild>
-                      <Link href="/">Início</Link>
-                    </TabNav.Link>
-                    <TabNav.Link asChild>
-                      <Link href="/musica">Música</Link>
-                    </TabNav.Link>
-                    <TabNav.Link asChild>
-                      <Link href="/bio">
-                        Bio&nbsp;e&nbsp;
-                        <em className="not-italic" lang="en">
-                          Releases
-                        </em>
-                      </Link>
-                    </TabNav.Link>
-                    <TabNav.Link asChild>
-                      <Link href="/midia">Mídia</Link>
-                    </TabNav.Link>
-                  </TabNav.Root>
-                  <SocialMediaLinks />
-                </Flex>
-              </Container>
-            </header>
-          </Box>
-          <main id="content" role="main" tabIndex={-1}>
-            {children}
-          </main>
-          <footer>
-            <Container>
-              <Flex justify="between">
-                <p>
-                  2008-2024 Jimmy Andrade Produção Fonográfica, Musical e
-                  Marketing.
-                </p>
-                <Flex asChild>
-                  <SocialMediaLinks />
-                </Flex>
-              </Flex>
-            </Container>
-          </footer>
+    <Theme asChild appearance="dark" hasBackground accentColor="red">
+      <html className={albertSans.variable} lang="pt-br">
+        <body className="font-sans overscroll-y-none">
+          <SiteHeader />
+          <MainContent>{children}</MainContent>
+          <SiteFooter />
         </body>
       </html>
     </Theme>
