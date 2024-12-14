73be4e88c3f75cd677e93994ff5d504442712b1b
Jimmy Andrade <contato@jimmyandrade.com>
2024-12-14 16:26:00 -0300

    feat: add Container component for layout management

---
+++ apps/dotmusic/src/libs/shared/ui/Container.tsx
@@ -0,0 +1,19 @@
+import {
+  Container as PrimitiveContainer,
+  type ContainerProps,
+} from '@radix-ui/themes';
+
+export const Container = ({ children, ...props }: ContainerProps) => (
+  <PrimitiveContainer
+    size={{
+      initial: '1',
+      xs: '1',
+      sm: '2',
+      md: '3',
+      lg: '4',
+    }}
+    {...props}
+  >
+    {children}
+  </PrimitiveContainer>
+);
