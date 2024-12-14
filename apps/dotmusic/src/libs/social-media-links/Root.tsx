b5fc40726a0a80d490172c5dbfff3daf7a65701f
Jimmy Andrade <contato@jimmyandrade.com>
2024-12-14 16:27:27 -0300

    feat: add SocialMediaLinks component with Instagram, TikTok, YouTube, Spotify, and Apple Music links

---
+++ apps/dotmusic/src/libs/social-media-links/Root.tsx
@@ -0,0 +1,82 @@
+import { AccessibleIcon, Flex, type FlexProps } from '@radix-ui/themes';
+import { InstagramIcon } from './InstagramIcon';
+import { TikTokIcon } from './TikTokIcon';
+import { IconButton } from './IconButton';
+
+export const Root = ({
+  asChild = true,
+  className,
+  gap = '4',
+  ...props
+}: FlexProps) => (
+  <Flex asChild={asChild} className={className} gap={gap} {...props}>
+    <ul>
+      <li>
+        <IconButton asChild>
+          <a
+            href="https://instagram.com/jimmyandrade"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <AccessibleIcon label="Instagram">
+              <InstagramIcon className="social-media-icon" />
+            </AccessibleIcon>
+          </a>
+        </IconButton>
+      </li>
+      <li>
+        <IconButton asChild>
+          <a
+            href="https://tiktok.com/@jimmyandrademusic"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <AccessibleIcon label="TikTok">
+              <TikTokIcon className="social-media-icon" />
+            </AccessibleIcon>
+          </a>
+        </IconButton>
+      </li>
+      <li>
+        <IconButton asChild>
+          <a
+            href="https://youtube.com/jimmyandrade"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <AccessibleIcon label="YouTube">
+              <InstagramIcon className="social-media-icon" />
+            </AccessibleIcon>
+          </a>
+        </IconButton>
+      </li>
+
+      <li>
+        <IconButton asChild>
+          <a
+            href="https://open.spotify.com/intl-pt/artist/5cXCuau8jCVmsRoFYj5oGB"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <AccessibleIcon label="Spotify">
+              <InstagramIcon className="social-media-icon" />
+            </AccessibleIcon>
+          </a>
+        </IconButton>
+      </li>
+      <li>
+        <IconButton asChild>
+          <a
+            href="https://music.apple.com/br/artist/jimmy-andrade/678083982"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <AccessibleIcon label="Apple Music">
+              <InstagramIcon className="social-media-icon" />
+            </AccessibleIcon>
+          </a>
+        </IconButton>
+      </li>
+    </ul>
+  </Flex>
+);
---
+++ apps/dotmusic/src/libs/social-media-links/index.ts
@@ -0,0 +1,7 @@
+import { InstagramIcon } from './InstagramIcon';
+import { Root } from './Root';
+
+export const SocialMediaLinks = {
+  InstagramIcon,
+  Root,
+};
