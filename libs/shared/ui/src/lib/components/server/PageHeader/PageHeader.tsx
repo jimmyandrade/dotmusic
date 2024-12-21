d2a8cd0f51eaf68099620f6d50d2633b7b724ab8
Jimmy Andrade <contato@jimmyandrade.com>
2024-12-21 17:22:09 -0300

    feat: add PageHeader component to shared UI library

---
+++ libs/shared/ui/src/lib/components/server/PageHeader/PageHeader.tsx
@@ -0,0 +1,23 @@
+import { type ReactNode } from 'react';
+import { Container } from '../Container';
+
+export interface PageHeaderProps extends React.HTMLAttributes<HTMLElement> {
+  children: ReactNode;
+}
+
+export const PageHeader = ({ children, ...props }: PageHeaderProps) => (
+  <header {...props}>
+    <Container
+      pt={{
+        initial: '4',
+        xs: '5',
+        sm: '6',
+        md: '7',
+        lg: '8',
+        xl: '9',
+      }}
+    >
+      {children}
+    </Container>
+  </header>
+);
