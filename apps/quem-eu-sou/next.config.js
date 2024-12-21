aca673bb6d2fa15c8d0d2679c9c7a2448d3004a8
Jimmy Andrade <contato@jimmyandrade.com>
2024-12-21 17:20:20 -0300

    feat: add remote pattern config for Google Images at Next.js

--- apps/quem-eu-sou/next.config.js
+++ apps/quem-eu-sou/next.config.js
@@ -7,6 +7,17 @@ const { composePlugins, withNx } = require('@nx/next');
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'yt3.googleusercontent.com',
+        port: '',
+        pathname: '**',
+        search: '',
+      },
+    ],
+  },
   nx: {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
