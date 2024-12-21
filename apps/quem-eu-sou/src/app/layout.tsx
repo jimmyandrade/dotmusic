2946a27d39478c680d14e37bec04b1866fec34a0
Jimmy Andrade <contato@jimmyandrade.com>
2024-12-21 17:23:38 -0300

    feat(layout): implement RootLayout with theme, navigation, and footer components

--- apps/quem-eu-sou/src/app/layout.tsx
+++ apps/quem-eu-sou/src/app/layout.tsx
@@ -1,4 +1,16 @@
+import { Em, Heading, Theme } from '@radix-ui/themes';
+import '@radix-ui/themes/styles.css';
 import './global.css';
+import Link from 'next/link';
+import {
+  HeaderHomeLink,
+  MainContent,
+  SiteFooter,
+  SiteHeader,
+  SiteNavigation,
+  TabNavLink,
+} from '@jimmyandrade/ui/server';
+import { BrandText } from '../components/BrandText';
 
 export const metadata = {};
 
@@ -8,10 +20,40 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html>
-      <body>
-        <main>{children}</main>
-      </body>
-    </html>
+    <Theme asChild appearance="light" hasBackground accentColor="purple">
+      <html>
+        <body>
+          <SiteHeader>
+            <HeaderHomeLink>
+              <BrandText />
+            </HeaderHomeLink>
+            <Heading as="h1" size="1">
+              <Link href="/"></Link>
+            </Heading>
+            <SiteNavigation aria-label="Navegação principal">
+              <TabNavLink asChild>
+                <Link href="/">Início</Link>
+              </TabNavLink>
+              <TabNavLink asChild>
+                <Link href="/sobre">Sobre</Link>
+              </TabNavLink>
+              <TabNavLink asChild>
+                <Link href="/musica">Música</Link>
+              </TabNavLink>
+              <TabNavLink asChild>
+                <Link href="/disco">Disco</Link>
+              </TabNavLink>
+              <TabNavLink asChild>
+                <Link href="/tour">
+                  <Em lang="en">Tour</Em>
+                </Link>
+              </TabNavLink>
+            </SiteNavigation>
+          </SiteHeader>
+          <MainContent>{children}</MainContent>
+          <SiteFooter />
+        </body>
+      </html>
+    </Theme>
   );
 }
