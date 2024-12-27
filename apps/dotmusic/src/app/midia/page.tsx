7118702d0ff956b219039c7fc8a4e60fb5cbab87
Jimmy Andrade <contato@jimmyandrade.com>
2024-12-27 13:47:51 -0300

    feat: create `PressPage` component for the `/midia` route

---
+++ apps/dotmusic/src/app/midia/page.tsx
@@ -0,0 +1,11 @@
+import { PageHeader, PageHeading } from '@jimmyandrade/ui/server';
+
+export default function PressPage() {
+  return (
+    <article id={PressPage.name}>
+      <PageHeader>
+        <PageHeading>Mídia</PageHeading>
+      </PageHeader>
+    </article>
+  );
+}
