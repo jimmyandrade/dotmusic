81014760689c80e27918b5b358d9d93216244fef
Jimmy Andrade <contato@jimmyandrade.com>
2024-12-27 13:09:58 -0300

    feat: refactor SearchPage component to enhance search functionality and error handling

--- apps/dotmusic/src/app/buscar/page.tsx
+++ apps/dotmusic/src/app/buscar/page.tsx
@@ -1,29 +1,106 @@
-import { Container } from '@jimmyandrade/ui/server';
+import {
+  Container,
+  PageHeader,
+  PageHeading,
+  VisuallyHidden,
+} from '@jimmyandrade/ui/server';
+import {
+  Button,
+  Callout,
+  Card,
+  Flex,
+  Heading,
+  Quote,
+  Text,
+  TextField,
+} from '@radix-ui/themes';
+import Link from 'next/link';
+import { fetchGoogleCustomSearchResults } from '../../libs/search/data-access';
 
-export default async function SearchPage() {
-  const customSearchBaseURL = 'https://www.googleapis.com/customsearch/v1';
-  const url = new URL(customSearchBaseURL);
+export interface SearchPageProps {
+  q?: string;
+}
 
-  if (typeof process.env.GOOGLE_CUSTOM_SEARCH_API_KEY === 'undefined') {
-    throw new Error('GOOGLE_CUSTOM_SEARCH_API_KEY is not defined');
+export default async function SearchPage({ searchParams }) {
+  try {
+    const query = searchParams.q;
+    const data = await fetchGoogleCustomSearchResults(query);
+
+    return (
+      <article id={SearchPage.name}>
+        <PageHeader>
+          <VisuallyHidden>
+            {query === '' ? (
+              <>
+                Resultados da busca por <Quote>{query}</Quote>
+              </>
+            ) : (
+              <PageHeading>Buscar</PageHeading>
+            )}
+          </VisuallyHidden>
+        </PageHeader>
+        <Container>
+          <Flex asChild gapX="2">
+            <form>
+              <TextField.Root
+                className="flex-grow"
+                defaultValue={query}
+                id="q"
+                name="q"
+                placeholder="Pesquisar em JimmyAndrade.music, JYVERSO e sites relacionados"
+                size="3"
+                type="search"
+              />
+              <Button size="3" type="submit">
+                Pesquisar
+              </Button>
+            </form>
+          </Flex>
+          {typeof data.items !== 'undefined' && (
+            <ul>
+              {data.items.map((item) => (
+                <li data-kind={item.kind} key={item.formattedUrl}>
+                  <Card asChild mt="4">
+                    <Link href={item.formattedUrl} title={item.title}>
+                      <Text
+                        color="gray"
+                        mb="4"
+                        size="1"
+                        dangerouslySetInnerHTML={{
+                          __html: item.htmlFormattedUrl,
+                        }}
+                      />
+                      <Heading
+                        dangerouslySetInnerHTML={{ __html: item.htmlTitle }}
+                        size={'3'}
+                      />
+                      <Text
+                        as="p"
+                        aria-label={item.snippet}
+                        dangerouslySetInnerHTML={{ __html: item.htmlSnippet }}
+                      />
+                      {/* {JSON.stringify(item.pagemap)} */}
+                    </Link>
+                  </Card>
+                </li>
+              ))}
+            </ul>
+          )}
+        </Container>
+      </article>
+    );
+  } catch (error) {
+    return (
+      <Callout.Root>
+        <Callout.Text>
+          {error !== null &&
+          typeof error === 'object' &&
+          'message' in error &&
+          typeof error.message === 'string'
+            ? error.message
+            : 'Não é possível buscar resultados no momento.'}
+        </Callout.Text>
+      </Callout.Root>
+    );
   }
-
-  if (typeof process.env.GOOGLE_CUSTOM_SEARCH_ENGINE_ID === 'undefined') {
-    throw new Error('GOOGLE_CUSTOM_SEARCH_ENGINE_ID is not defined');
-  }
-
-  url.searchParams.append('key', process.env.GOOGLE_CUSTOM_SEARCH_API_KEY);
-  url.searchParams.append('cx', process.env.GOOGLE_CUSTOM_SEARCH_ENGINE_ID);
-  url.searchParams.append('q', 'jimmy');
-
-  const response = await fetch(url.toString());
-  const data = await response.json();
-  const { ...props } = data;
-
-  return (
-    <div id={SearchPage.name}>
-      <Container />
-      <pre>{JSON.stringify(props, null, 2)}</pre>
-    </div>
-  );
 }
