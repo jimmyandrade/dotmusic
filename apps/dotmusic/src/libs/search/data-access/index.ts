7636a95ef7d997c99bbd7505f07b62dcd9359c5b
Jimmy Andrade <contato@jimmyandrade.com>
2024-12-27 13:04:58 -0300

    feat: implement `fetchGoogleCustomSearchResults` function for Google Custom Search API

---
+++ apps/dotmusic/src/libs/search/data-access/index.ts
@@ -0,0 +1,22 @@
+import { CustomSearchError } from '../model';
+
+export const fetchGoogleCustomSearchResults = async (query: string) => {
+  const customSearchBaseURL = 'https://www.googleapis.com/customsearch/v1';
+  const url = new URL(customSearchBaseURL);
+
+  if (typeof process.env.GOOGLE_CUSTOM_SEARCH_API_KEY === 'undefined') {
+    throw new Error(CustomSearchError.MISSING_API_KEY);
+  }
+
+  if (typeof process.env.GOOGLE_CUSTOM_SEARCH_ENGINE_ID === 'undefined') {
+    throw new Error(CustomSearchError.MISSING_ENGINE_ID);
+  }
+
+  url.searchParams.append('key', process.env.GOOGLE_CUSTOM_SEARCH_API_KEY);
+  url.searchParams.append('cx', process.env.GOOGLE_CUSTOM_SEARCH_ENGINE_ID);
+  url.searchParams.append('q', query);
+
+  const response = await fetch(url.toString());
+  const data = await response.json();
+  return data;
+};
