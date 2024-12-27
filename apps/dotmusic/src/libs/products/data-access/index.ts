e1d49e8265d8f87ef5866bf8e85db8aeeb50b90c
Jimmy Andrade <contato@jimmyandrade.com>
2024-12-27 13:09:45 -0300

    feat: implement `fetchReservaInkProducts` function to fetch and parse product data from Reserva Ink

---
+++ apps/dotmusic/src/libs/products/data-access/index.ts
@@ -0,0 +1,18 @@
+import { parse } from 'csv-parse/sync';
+import { mapProducts, ParsedRawData } from '../model/mapProduct';
+
+export const fetchReservaInkProducts = async () => {
+  const response = await fetch(
+    'https://reserva.ink/user/dashboard/export_csv.facebook_store_feed_172246',
+  );
+
+  const csvRawText = await response.text();
+
+  const parsedRawData: ParsedRawData = parse(csvRawText, {
+    columns: true,
+    delimiter: ',',
+    skip_empty_lines: true,
+  });
+
+  return mapProducts(parsedRawData);
+};
