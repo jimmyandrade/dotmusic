1d860a7b11715fdd8dde21febe802ce021ca6c09
Jimmy Andrade <contato@jimmyandrade.com>
2025-07-15 21:57:12 -0300

    feat: enhance `Container` with `forwardRef` and improved classes
    Refactored the Container component to use React's forwardRef for better ref forwarding support. Added classNames utility for merging classes and included default container styling for consistent layout.

--- src/libs/shared/ui/components/server/Container/Container.tsx
+++ src/libs/shared/ui/components/server/Container/Container.tsx
@@ -1,24 +1,35 @@
 import {
-  Container as PrimitiveContainer,
   type ContainerProps,
+  Container as PrimitiveContainer,
 } from '@radix-ui/themes';
+import classNames from 'classnames';
+import { forwardRef } from 'react';
 
 export type { ContainerProps };
 
-export const Container = ({ asChild, children, className = "", ...props }: ContainerProps) => (
-  <PrimitiveContainer
-    asChild={asChild}
-    className={className}
-    size={{
-      initial: '1',
-      xs: '1',
-      sm: '2',
-      md: '3',
-      lg: '4',
-    }}
-    {...props}
-  >
-    {children}
-  </PrimitiveContainer>
+export const Container = forwardRef<HTMLDivElement, ContainerProps>(
+  (
+    { asChild = false, children, className = '', ...props }: ContainerProps,
+    forwardedRef,
+  ) => (
+    <PrimitiveContainer
+      asChild={asChild}
+      className={classNames(
+        'container max-w-7xl mx-auto px-2 sm:px-6 lg:px-8',
+        className,
+      )}
+      size={{
+        initial: '1',
+        xs: '1',
+        sm: '2',
+        md: '3',
+        lg: '4',
+      }}
+      ref={forwardedRef}
+      {...props}
+    >
+      {children}
+    </PrimitiveContainer>
+  ),
 );
 Container.displayName = 'Container';
